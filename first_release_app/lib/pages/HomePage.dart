a24c789f0d3d23873a038340fd3e75cce460f83f
전길원 <>
2022-07-21 23:43:27 +0900

    1~45까지 번호 랜덤 출력 <완성> 원하는 숫자 8개중 6개 출력 <완성> 게임회차 정해서 한 번에 여러개 출력하게 하기 <미완성>

--- first_release_app/lib/pages/HomePage.dart
+++ first_release_app/lib/pages/HomePage.dart
@@ -1,30 +1,87 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class lottoHome extends StatelessWidget {
+class lottoHome extends StatefulWidget {
   const lottoHome({Key? key}) : super(key: key);
 
   @override
+  State<lottoHome> createState() => _lottoHome_State();
+}
+
+class _lottoHome_State extends State<lottoHome> {
+  List<int> _lottoSet =[];
+  //전체 번호 중 랜덤하게 추출하는 set를 담을 리스트 생성
+  List<int> _wantSet = [];
+  //원하는 번호 중 랜덤하게 추출하는 set을 담을 리스트 생성
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: Text("로또 번호 추첨기"),
-            backgroundColor: Colors.lime,
-            floating: true,
-            pinned: false,
+      appBar: AppBar(
+        title: Text("Lottery Number Generate"),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+              height: 20
           ),
-          SliverList(delegate: SliverChildListDelegate(
-            [
-              IconButton(
-                icon: Icon(Icons.shuffle_rounded),
-                onPressed: (){
-                  
-                },
-              ),
-            ],
-          )),
+          Center(child: ElevatedButton(onPressed: (){
+            setState(() {
+              //버튼을 누르면
+              List<int> lottoSet = [];
+              //번호를 담을 lottoSet을 만든다
+              lottoSet.clear();
+              //여러 번 누를 수 있기 때문에 누를 때마다 clear할 수 있게 만듦
+              while(true){
+                var rnd = Random().nextInt(45)+1;
+                //1~45까지 수에서 랜덤하게 하나씩 뽑음
+
+                if (!lottoSet.contains(rnd)){
+                  //lottoSet에 랜덤 숫자가 없다? == 중복되지 않은 숫자
+                  lottoSet.add(rnd);
+                  //lottoSet에 넣음
+                }
+                if(lottoSet.length == 6){
+                  lottoSet.sort();
+                  //lottoSet의 개수가 원하는 만큼 되면 정렬하고 탈출
+                  break;
+                }
+              }
+              _lottoSet = lottoSet;
+              //누를 때마다 업데이트를 해준다 출력할 때 사용하는 변수 _lottoSet에
+            });
+          }, child: Text("1~45 번호 중 랜덤하여 생성",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w200))),),
+          Text("${_lottoSet.toString()}",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+          //=========모든 번호 중 랜덤하게 생성하게 하는 것 ===========
+          SizedBox(height: 100),
+          Center(child:ElevatedButton(
+            child: Text("1,2,17,19,25,28,33,37 중 번호 생성"),
+            onPressed: (){
+              setState(() {
+                List<int> wantSet =[1,2,17,19,25,28,33,37];
+                _wantSet.clear();
+                List<int> tmp = [];
+                tmp.clear();
+                while(true){
+                  var rnd = Random().nextInt(8)+1;
+                  if (!tmp.contains(rnd)){
+                    //tmp안에 랜덤한 인덱스 하나 뽑은게 없다면
+                    _wantSet.add(wantSet[rnd-1]);
+                    tmp.add(rnd);
+                  }
+                  if(_wantSet.length == 6){
+                    _wantSet.sort();
+                    break;
+                  }
+                }
+              });
+            },
+          ),
+
+          ),
+          Text("${_wantSet.toString()}"),
         ],
+
       ),
     );
   }
